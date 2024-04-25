8264734765f36c9e724306336a2fc71d15e35340
Colin Baker <colin_baker@brown.edugit config --global user.name Cbaker37>
2024-04-25 10:26:45 -0400

    comments

--- model/model.py
+++ model/model.py
@@ -12,42 +12,99 @@ BATCH_SZ = 32
 # add more weight to positives in BCE loss
 
 class SelfAttn(tf.keras.layers.Layer):
+    '''
+    Standard multi-headed self attention layer
+    '''
 
     def __init__(self, in_shape, out_shape, num_heads):
+        '''
+        Initialize self attention layer.
+
+        Parameters:
+        -----------
+        in_shape: input window size
+        out_shape: output window size
+        num_heads: number of self attention heads
+
+        Returns:
+        --------
+        None
+        '''
         super(SelfAttn, self).__init__()
 
         self.in_shape = in_shape
         self.out_shape = out_shape
         self.num_heads = num_heads
 
-    def build(self, input_shape):
-        # doesn't work rn, gives weights of size batch_sz, input (while we want input, output)
+    def build(self, input_shape=None):
+        '''
+        Build weights for the self attention layer.
 
+        Parameters:
+        -----------
+        None
+
+        Returns:
+        --------
+        None
+        '''
+        # make list of weight matrices, one for each head (K, Q, V)
         self.K = [self.add_weight(f'K{i}', (self.in_shape, self.out_shape), trainable=True) for i in range(self.num_heads)]
         self.Q = [self.add_weight(f'Q{i}', (self.in_shape, self.out_shape), trainable=True) for i in range(self.num_heads)]
         self.V = [self.add_weight(f'V{i}', (self.in_shape, self.out_shape), trainable=True) for i in range(self.num_heads)]
 
+        # weight matrix for combining head information
         self.W = self.add_weight(f'W', (self.out_shape*self.num_heads, self.out_shape))
 
     def call(self, inputs):
+        '''
+        Self attention layer call, with masking as per HW5.
+
+        Parameters:
+        -----------
+        inputs - inputs over which to compute self attention
+
+        Returns:
+        --------
+        Result of self attention computation
+        '''
 
         keys, queries, values = inputs, inputs, inputs
 
+        # computing keys, queries, values
         K = [keys @ k for k in self.K]
         Q = [queries @ q for q in self.Q]
         V = [values @ v for v in self.V]
 
+        # computing mask
         mask_vals = np.triu(np.ones((Q[0].shape[-2], K[0].shape[-2])) * np.NINF, k=1)
         mask = tf.convert_to_tensor(value=mask_vals, dtype=tf.float32)
         atten_mask = tf.tile(tf.reshape(mask, [-1, Q[0].shape[-2], K[0].shape[-2]]), [tf.shape(input=K[0])[0], 1, 1])
 
+        # computing a list of single head attentions, concatenated as a tensor
         single_head_attns = tf.concat([tf.nn.softmax(((q @ tf.transpose(k, [0, 2, 1]))/math.sqrt(k.shape[-1])) + atten_mask) @ v for q, k, v in zip(Q, K, V)], axis=-1)
 
+        # combine single head information via dense layer
         return single_head_attns @ self.W
 
 class Model(tf.keras.Model):
+    '''
+    Our take on the SATORI model!
+    '''
 
     def __init__(self, seq_len):
+        '''
+        Set up all necessary layers for model call step.
+
+        Parameters:
+        -----------
+        seq_len - the length of the input sequences (necessary to deal with 
+        None batch size during training)
+
+        Returns:
+        --------
+        None
+        '''
 
         super(Model, self).__init__()
 
@@ -64,23 +121,53 @@ class Model(tf.keras.Model):
         self.num_dense = 2
         self.dense = []
 
+        # if we change padding to VALID, will have to compute conv_out_shape 
+        # successively for each layer so that we get a correct out_shape to 
+        # pass into self attention layer weight build
         for _ in range(self.num_convolutions):
+            # note that the 4 here is necessary to make this convolution 
+            # essentially 1D, but the 20 and 5 are arbitrary hyperparameters 
+            # (although the 20 param should match what we expect to be our 
+            # motif lengths, at least for first conv layer)
             self.convolutions.append(tf.keras.layers.Convolution2D(5, (4, 20), (1, 1), padding="SAME"))
 
+        # arbitrary second conv
         self.convolutions.append(tf.keras.layers.Convolution2D(1, (4, 20), (1, 1), padding="SAME"))
         self.convolutions.append(tf.keras.layers.MaxPool2D((1, 20), (1, 5)))
+
+        # 20 and 5 are coming from the max pooling layer here, given prior two
+        # convolutions both keep same dimensions
         self.conv_out_shape = (self.seq_len - 20)//5 + 1
 
+        # 30 is an arbitrary hyperparam here, feel free to change
+        # 5 is also arbitrary (num of heads)
         self.self_attns.append(SelfAttn(self.conv_out_shape, 30, 5))
         for _ in range(self.num_self_attns):
             self.self_attns.append(SelfAttn(30, 30, 5))
 
+        # more arbitrary hyperparams
         for _ in range(self.num_dense):
             self.dense.append(tf.keras.layers.Dense(10, activation='leaky_relu'))
         
+        # can do 2, softmax (works easier with loss function I've put in) or
+        # 1, sigmoid...both should theoretically work, but may have to code 
+        # custom loss func as keras BCEs are a little finnicky
         self.dense.append(tf.keras.layers.Dense(2, activation='softmax'))
 
     def call(self, inputs, training=False):
+        '''
+        Call function for our SATORI model. Does 1D convolutions, gets rid of 
+        the channel dimension, then does self attention and dense layers. 
+
+        Parameters:
+        -----------
+        inputs - inputs over which to predict label distribution
+        training - flag we might use if we do regularization
+
+        Returns:
+        --------
+        Resulting enhancer label distribution
+        '''
 
         x = tf.one_hot(inputs, 4, axis=1)
 
@@ -90,6 +177,7 @@ class Model(tf.keras.Model):
         for conv in self.convolutions:
             x = conv(x)
 
+        # removing "channel" for conv2D
         x = tf.reshape(x, (-1, 4, self.conv_out_shape))
 
         for self_attn in self.self_attns:
@@ -104,11 +192,15 @@ class Model(tf.keras.Model):
 
 if __name__ == '__main__':
 
+    # sequence length 300 is arbitrary here
     model = Model(300)
     sim_data = DataSimulator()
 
+    # motifs also arbitrary, maybe try pushing up to match convolution length 
+    # dim of 20
     sim_data.add_interactions([('AAAAAAAA', 'CCCCCCCC'), ('CCCCCCCC', 'TTTTTTTT')])
 
+    # rest should be fairly self explanatory here
     pos, neg, pos_labels, neg_labels = sim_data.simulate(300, 10000)
 
     train_X = tf.concat([pos, neg], axis=0)
@@ -116,7 +208,6 @@ if __name__ == '__main__':
 
     inds = tf.random.shuffle(tf.range(train_X.shape[0]))
 
-    print(train_y)
     train_y = tf.one_hot(train_y,2)
 
     model.call(train_X)
--- model/simulator.py
+++ model/simulator.py
@@ -5,38 +5,143 @@ from tqdm import tqdm
 import networkx as nx
 import random
 
+# for categorical encoding of sequences (prior to OHE)
 MAP_ENCODE = {'A' : 0, 'C' : 1, 'G' : 2, 'T' : 3}
 MAP_DECODE = {'A' : 0, 'C' : 1, 'G' : 2, 'T' : 3}
 
+
 def map_encode(seq : str):
+    '''
+    Turn sequence in {A, C, G, T}^+ into a list of {0, 1, 2, 3}^+.
+
+    Parameters:
+    -----------
+    seq - sequence of {A, C, G, T} as a string
+
+    Return:
+    -------
+    A list of {0, 1, 2, 3}
+    '''
     split_seq = [*seq]
     return [MAP_ENCODE[x] for x in split_seq]
 
 def map_decode(seq : list[int]):
+    '''
+    Turn a list sequence in {0, 1, 2, 3}^+ into a string in {A, C, G, T}^+.
+
+    Parameters:
+    -----------
+    seq - a list of {0, 1, 2, 3}
+
+    Return:
+    -------
+    A sequence of {A, C, G, T} as a string
+    '''
     split_seq = [MAP_DECODE[x] for x in seq]
     return ''.join(split_seq)
 
 class DataSimulator():
+    '''
+    A class for simulating motif interaction data.
+    '''
 
     def __init__(self):
+        '''
+        Initialize simulator class with an interaction graph
+
+        Parameters:
+        -----------
+        None
+
+        Returns:
+        --------
+        None
+        '''
         self.interaction_graph = nx.Graph()
 
     def add_motif(self, motif : str):
+        '''
+        Add a motif to the interaction graph.
+
+        Parameters:
+        -----------
+        motif - motif to add to the interaction graph as a string
+
+        Returns:
+        --------
+        None
+        '''
         self.interaction_graph.add_node(motif)
     
     def add_interaction(self, motif1 : str, motif2 : str):
+        '''
+        Add a motif interaction to the interaction graph.
+
+        Parameters:
+        -----------
+        motif1, motif2 - the two motifs to add to the interaction graph with an 
+        undirected edge
+
+        Returns:
+        --------
+        None
+        '''
         self.interaction_graph.add_edge(motif1, motif2)
     
     def add_motifs(self, motifs : list[str]):
+        '''
+        Add multiple motifs to the interaction graph.
+
+        Parameters:
+        -----------
+        motifs - list of motifs to add to the graph
+
+        Returns:
+        --------
+        None
+        '''
         self.interaction_graph.add_nodes_from(motifs)
     
     def add_interactions(self, motif_ints : list[tuple[str, str]]):
+        '''
+        Add multiple motif interaction pairs to the interaction graph.
+
+        Parameters:
+        -----------
+        motif_ints - list of motif interaction pairs to add to the graph
+
+        Returns:
+        --------
+        None
+        '''
         self.interaction_graph.add_edges_from(motif_ints)
 
     def sample_node_pair(self):
+        '''
+        Sample a random pair of nodes from the graph.
+
+        Parameters:
+        -----------
+        None
+
+        Returns:
+        --------
+        A tuple of motifs from the interaction graph
+        '''
         return random.choice([x for x in self.interaction_graph.nodes()]), random.choice([x for x in self.interaction_graph.nodes()])
 
     def gen_neg(self, len : int):
+        '''
+        Generate a random sequence in {A, C, G, T}^{len}.
+
+        Parameters:
+        -----------
+        len - the length of the random sequence to generate
+
+        Returns:
+        --------
+        The generated random sequence
+        '''
         seq = ''
         if len == 0:
             return ''
@@ -45,35 +150,80 @@ class DataSimulator():
         return seq
     
     def seqs_to_tens(self, seqs : list[str]):
+        '''
+        Convert a list of sequences in {A, C, G, T} to a tensor according to the
+        categorical maps defined above.
+
+        Parameters:
+        -----------
+        seqs - list of sequences to convert
+
+        Returns:
+        --------
+        A tensor of dimension (num_seqs, len_seq) filled with {0, 1, 2, 3}
+        '''
         encoded_seq = [map_encode(x) for x in seqs]
         return tf.convert_to_tensor(encoded_seq)
 
     def simulate(self, seq_lens : int, num_seqs : int):
+        '''
+        Simulate 1/2*num_seqs positive sequences and 1/2*num_seqs negative
+        sequences, all of length seq_lens. Here, we define positive sequence as 
+        one that contains a pair of motifs connected by an edge in the graph, 
+        and a negative sequence as either random or containing a pair of 
+        unconnected motifs.
+
+        Parameters:
+        -----------
+        seq_lens - length of sequences to generate
+        num_seqs - number of sequences to generate
+
+        Returns:
+        --------
+        The positive sequences as a tensor, the negative sequences as a tensor, 
+        and labels for each tensor (all ones for positive tensor, all zeros for 
+        negative tensor)
+        '''
         pos_data = []
         num_ints = len(self.interaction_graph.edges())
+
+        # for each pair of interacting motifs
         for motif1, motif2 in self.interaction_graph.edges():
+            # generate a proportional number of positive sequences
             for _ in range((num_seqs // 2) // num_ints):
+                # randomly shuffle motifs (undirected edge model)
                 assert(seq_lens - len(motif1) - len(motif2) > 0)
                 m1 = motif1
                 m2 = motif2
                 if (random.choice([0, 1]) == 1):
                     m1 = motif1
                     m2 = motif2   
+                
+                # choose positions in the new sequence at which to place both 
+                # motifs
                 start1 = random.randint(0, seq_lens - (len(m1) + len(m2)) - 1)
                 start2 = random.randint(start1 + len(m1), seq_lens - len(m2))
             
+                # fill in the gaps with negative sequence data
                 pos_data.append(self.gen_neg(start1) + m1 + self.gen_neg(start2 - start1 - len(m1)) + m2 + self.gen_neg(seq_lens - start2 - len(m2)))
         
         neg_data = []
         rand_neg = (num_seqs - len(pos_data))//2
         sys_neg = (num_seqs - len(pos_data) - rand_neg)
+
+        # generate half the negative data as random sequences
         for _ in range(rand_neg):
             neg_data.append(self.gen_neg(seq_lens))
 
+        # generate the other half by the same procedure as positives, but only 
+        # for motif pairs NOT in the edge set
         for _ in range(sys_neg):
+            # find a negative motif pair
             motif1, motif2 = self.sample_node_pair()
             while (motif1, motif2) in [x for x in self.interaction_graph.edges()]:
                 motif1, motif2 = self.sample_node_pair()
+
+            # same as for positive motif pair
             m1 = motif1
             m2 = motif2
             if (random.choice([0, 1]) == 1):
@@ -84,5 +234,4 @@ class DataSimulator():
         
             neg_data.append(self.gen_neg(start1) + m1 + self.gen_neg(start2 - start1 - len(m1)) + m2 + self.gen_neg(seq_lens - start2 - len(m2)))
             
-
         return self.seqs_to_tens(pos_data), self.seqs_to_tens(neg_data), tf.ones(len(pos_data), dtype=tf.uint8), tf.zeros(len(neg_data), dtype=tf.uint8)
