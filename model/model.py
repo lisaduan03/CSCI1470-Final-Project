0121913722340c3bbdc1d2a6f8c8c81d30a9295d
Colin Baker <colin_baker@brown.edugit config --global user.name Cbaker37>
2024-04-25 00:19:48 -0400

    mostly working simulation

--- model/model.py
+++ model/model.py
@@ -9,20 +9,23 @@ from simulator import DataSimulator
 NUM_EPOCHS = 10
 BATCH_SZ = 32
 
+# add more weight to positives in BCE loss
+
 class SelfAttn(tf.keras.layers.Layer):
 
-    def __init__(self, out_shape, num_heads):
+    def __init__(self, in_shape, out_shape, num_heads):
         super(SelfAttn, self).__init__()
 
+        self.in_shape = in_shape
         self.out_shape = out_shape
         self.num_heads = num_heads
 
     def build(self, input_shape):
         # doesn't work rn, gives weights of size batch_sz, input (while we want input, output)
 
-        self.K = [self.add_weight(f'K{i}', (input_shape[-1], self.out_shape), trainable=True) for i in range(self.num_heads)]
-        self.Q = [self.add_weight(f'Q{i}', (input_shape[-1], self.out_shape), trainable=True) for i in range(self.num_heads)]
-        self.V = [self.add_weight(f'V{i}', (input_shape[-1], self.out_shape), trainable=True) for i in range(self.num_heads)]
+        self.K = [self.add_weight(f'K{i}', (self.in_shape, self.out_shape), trainable=True) for i in range(self.num_heads)]
+        self.Q = [self.add_weight(f'Q{i}', (self.in_shape, self.out_shape), trainable=True) for i in range(self.num_heads)]
+        self.V = [self.add_weight(f'V{i}', (self.in_shape, self.out_shape), trainable=True) for i in range(self.num_heads)]
 
         self.W = self.add_weight(f'W', (self.out_shape*self.num_heads, self.out_shape))
 
@@ -44,12 +47,14 @@ class SelfAttn(tf.keras.layers.Layer):
 
 class Model(tf.keras.Model):
 
-    def __init__(self):
+    def __init__(self, seq_len):
 
         super(Model, self).__init__()
 
         self.num_convolutions = 1
         self.convolutions = []
+
+        self.seq_len = seq_len
 
         self.flatten = tf.keras.layers.Flatten()
 
@@ -60,18 +65,20 @@ class Model(tf.keras.Model):
         self.dense = []
 
         for _ in range(self.num_convolutions):
-            self.convolutions.append(tf.keras.layers.Convolution2D(5, (4, 10), (1, 1), padding="SAME"))
+            self.convolutions.append(tf.keras.layers.Convolution2D(5, (4, 20), (1, 1), padding="SAME"))
 
-        self.convolutions.append(tf.keras.layers.Convolution2D(1, (4, 10), (1, 1), padding="SAME"))
-        self.convolutions.append(tf.keras.layers.MaxPool2D((1, 10), (1, 5)))
+        self.convolutions.append(tf.keras.layers.Convolution2D(1, (4, 20), (1, 1), padding="SAME"))
+        self.convolutions.append(tf.keras.layers.MaxPool2D((1, 20), (1, 5)))
+        self.conv_out_shape = (self.seq_len - 20)//5 + 1
 
+        self.self_attns.append(SelfAttn(self.conv_out_shape, 30, 5))
         for _ in range(self.num_self_attns):
-            self.self_attns.append(SelfAttn(30, 5))
+            self.self_attns.append(SelfAttn(30, 30, 5))
 
         for _ in range(self.num_dense):
             self.dense.append(tf.keras.layers.Dense(10, activation='leaky_relu'))
         
-        self.dense.append(tf.keras.layers.Dense(1, activation='sigmoid'))
+        self.dense.append(tf.keras.layers.Dense(2, activation='softmax'))
 
     def call(self, inputs, training=False):
 
@@ -83,27 +90,21 @@ class Model(tf.keras.Model):
         for conv in self.convolutions:
             x = conv(x)
 
-        print(x.shape)
-        x = tf.reshape(x, (x.shape[0], 4, -1))
-
-        print(x.shape)
+        x = tf.reshape(x, (-1, 4, self.conv_out_shape))
 
         for self_attn in self.self_attns:
             x = self_attn(x)
-        print(x.shape)
 
         x = self.flatten(x)
-        print(x.shape)
 
         for dense in self.dense:
             x = dense(x)
-        print(x.shape)
         
         return x
 
 if __name__ == '__main__':
 
-    model = Model()
+    model = Model(300)
     sim_data = DataSimulator()
 
     sim_data.add_interactions([('AAAAAAAA', 'CCCCCCCC'), ('CCCCCCCC', 'TTTTTTTT')])
@@ -115,6 +116,9 @@ if __name__ == '__main__':
 
     inds = tf.random.shuffle(tf.range(train_X.shape[0]))
 
+    print(train_y)
+    train_y = tf.one_hot(train_y,2)
+
     model.call(train_X)
 
     train_X = tf.gather(train_X, inds)
@@ -123,7 +127,7 @@ if __name__ == '__main__':
     model.compile(
         optimizer=tf.keras.optimizers.legacy.Adam(),
         loss=tf.keras.losses.BinaryCrossentropy(),
-        metrics=[tf.keras.metrics.Accuracy()]
+        metrics=[tf.keras.metrics.categorical_accuracy]
     )
 
     model.fit(train_X, train_y, BATCH_SZ, NUM_EPOCHS)
@@ -133,10 +137,16 @@ if __name__ == '__main__':
     test_X = tf.concat([test_pos, test_neg], axis=0)
     test_y = tf.concat([test_pos_labels, test_neg_labels], axis=0)
 
+    test_y = tf.one_hot(test_y,2)
+
     inds = tf.random.shuffle(tf.range(test_X.shape[0]))
 
     test_X = tf.gather(test_X, inds)
     test_y = tf.gather(test_y, inds)
 
-    model.test_on_batch(test_X, test_y)
+    print(model.test_on_batch(test_X, test_y))
+
+    print(test_y[0], model.call(tf.expand_dims(test_X[0], 0)))
+    print(test_y[5], model.call(tf.expand_dims(test_X[5], 0)))
+    print(test_y[7], model.call(tf.expand_dims(test_X[7], 0)))
         
--- model/simulator.py
+++ model/simulator.py
@@ -33,6 +33,9 @@ class DataSimulator():
     def add_interactions(self, motif_ints : list[tuple[str, str]]):
         self.interaction_graph.add_edges_from(motif_ints)
 
+    def sample_node_pair(self):
+        return random.choice([x for x in self.interaction_graph.nodes()]), random.choice([x for x in self.interaction_graph.nodes()])
+
     def gen_neg(self, len : int):
         seq = ''
         if len == 0:
@@ -49,16 +52,37 @@ class DataSimulator():
         pos_data = []
         num_ints = len(self.interaction_graph.edges())
         for motif1, motif2 in self.interaction_graph.edges():
-
             for _ in range((num_seqs // 2) // num_ints):
                 assert(seq_lens - len(motif1) - len(motif2) > 0)
-                start1 = random.randint(0, seq_lens - (len(motif1) + len(motif2)) - 1)
-                start2 = random.randint(start1 + len(motif1), seq_lens - len(motif2))
+                m1 = motif1
+                m2 = motif2
+                if (random.choice([0, 1]) == 1):
+                    m1 = motif1
+                    m2 = motif2   
+                start1 = random.randint(0, seq_lens - (len(m1) + len(m2)) - 1)
+                start2 = random.randint(start1 + len(m1), seq_lens - len(m2))
             
-                pos_data.append(self.gen_neg(start1) + motif1 + self.gen_neg(start2 - start1 - len(motif1)) + motif2 + self.gen_neg(seq_lens - start2 - len(motif2)))
+                pos_data.append(self.gen_neg(start1) + m1 + self.gen_neg(start2 - start1 - len(m1)) + m2 + self.gen_neg(seq_lens - start2 - len(m2)))
         
         neg_data = []
-        for _ in range(num_seqs - len(pos_data)):
+        rand_neg = (num_seqs - len(pos_data))//2
+        sys_neg = (num_seqs - len(pos_data) - rand_neg)
+        for _ in range(rand_neg):
             neg_data.append(self.gen_neg(seq_lens))
 
-        return self.seqs_to_tens(pos_data), self.seqs_to_tens(neg_data), tf.ones(len(pos_data)), tf.zeros(len(neg_data))
+        for _ in range(sys_neg):
+            motif1, motif2 = self.sample_node_pair()
+            while (motif1, motif2) in [x for x in self.interaction_graph.edges()]:
+                motif1, motif2 = self.sample_node_pair()
+            m1 = motif1
+            m2 = motif2
+            if (random.choice([0, 1]) == 1):
+                m1 = motif1
+                m2 = motif2   
+            start1 = random.randint(0, seq_lens - (len(m1) + len(m2)) - 1)
+            start2 = random.randint(start1 + len(m1), seq_lens - len(m2))
+        
+            neg_data.append(self.gen_neg(start1) + m1 + self.gen_neg(start2 - start1 - len(m1)) + m2 + self.gen_neg(seq_lens - start2 - len(m2)))
+            
+
+        return self.seqs_to_tens(pos_data), self.seqs_to_tens(neg_data), tf.ones(len(pos_data), dtype=tf.uint8), tf.zeros(len(neg_data), dtype=tf.uint8)
