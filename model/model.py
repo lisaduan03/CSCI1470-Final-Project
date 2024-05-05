ee63ecfe326e3dc74173bbbcde0e33e7b35f2469
Colin Baker <colin_baker@brown.edugit config --global user.name Cbaker37>
2024-05-05 12:40:13 -0400

    more extensive disconnected

--- model/model.py
+++ model/model.py
@@ -286,7 +286,7 @@ if __name__ == '__main__':
         ttk = np.transpose(np.genfromtxt('simulated_results/ttk_meme.txt'))
         neo = np.transpose(np.genfromtxt('simulated_results/neo38_meme.txt'))
         deaf = np.transpose(np.genfromtxt('simulated_results/deaf1_meme.txt'))
-        sim_data.add_interactions([('GTAGTCCGTCCCGTA', 'TTAGTCAGTCGATCA'), ('ATCGACGTAGCTAGC', 'GTAGTCCGTCCCGTA'), ('TTAGTCAGTCGATCA', 'ATCGACGTAGCTAGC')])
+        sim_data.add_interactions([('GTAGTCCGTCCCGTA', 'TTAGTCAGTCGATCA'), ('ATCGACGTAGCTAGC', 'GTAGTCCGTCCCGTA'), ('TTAGTCAGTCGATCA', 'ATCGACGTAGCTAGC'), ('CTCAGCTCTATTTTA', 'GTGGTCATGGGTTTT'), ('GTGGTCATGGGTTTT', 'GGTCCGCCCGAGCGG'), ('GGTCCGCCCGAGCGG', 'CTCAGCTCTATTTTA')])
 
         # rest should be fairly self explanatory here
         pos, neg, pos_labels, neg_labels = sim_data.simulate(300, 50000, True)
@@ -391,7 +391,7 @@ if __name__ == '__main__':
     history = model.fit(train_X, train_y, BATCH_SZ, NUM_EPOCHS, validation_data=(val_X, val_y))
     # model.show_figures(history.history)
 
-    model.save("simulated_results/disconnected_random/model_simulate_test.hd5")
+    model.save("simulated_results/large_disconnected_random/model_simulate_test.hd5")
 
     # model.show_figures(history.history)
     # testing
@@ -401,9 +401,9 @@ if __name__ == '__main__':
     test_y = tf.gather(test_y, inds)
 
     import pickle as pkl
-    with open('simulated_results/disconnected_random/test_X.pkl', 'wb') as f:
+    with open('simulated_results/large_disconnected_random/test_X.pkl', 'wb') as f:
         pkl.dump(test_X, f)
-    with open('simulated_results/disconnected_random/test_y.pkl', 'wb') as f:
+    with open('simulated_results/large_disconnected_random/test_y.pkl', 'wb') as f:
         pkl.dump(test_y, f)
 
     print(model.test_on_batch(test_X, test_y))
