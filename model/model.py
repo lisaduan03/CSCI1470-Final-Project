bba8b04b4d3b866dc19d5075c0844205a16e5a1c
Colin Baker <colin_baker@brown.edugit config --global user.name Cbaker37>
2024-05-05 12:20:27 -0400

    fixed model differences

--- model/model.py
+++ model/model.py
@@ -92,11 +92,12 @@ class SelfAttn(tf.keras.layers.Layer):
         # computing a list of single head attentions, concatenated as a tensor  
         # changed to for loop by JC to allow for attention score extraction 
         single_head_attns = []
-        pAttn_concat = tf.zeros([0,self.conv_out_shape, self.conv_out_shape])
+        pAttn_concat = tf.zeros([0,self.conv_out_shape, 2*self.conv_out_shape])
         for q, k, v in zip(Q, K, V):
             attn_scores = tf.nn.softmax(((q @ tf.transpose(k, [0, 2, 1]))/math.sqrt(k.shape[-1])) + atten_mask)
             outs = attn_scores @ v
             single_head_attns.append(outs)
+            attn_scores = tf.reshape(attn_scores, (-1, self.conv_out_shape, self.num_heads*self.conv_out_shape))
             pAttn_concat = tf.concat([pAttn_concat, attn_scores], axis=0)
         single_head_attns = tf.concat(single_head_attns, axis = -1)
 
@@ -219,7 +220,7 @@ class Model(tf.keras.Model):
         # LD: add biLSTM here
         # self.bidirectional_lstm(x)
 
-        x = x + positional_encoding(self.conv_out_shape, self.num_channels)
+        # x = x + positional_encoding(self.conv_out_shape, self.num_channels)
 
         for self_attn in self.self_attns:
             x, pAttn_concat = self_attn(x)  # don't need attention scores here 
@@ -266,7 +267,7 @@ if __name__ == '__main__':
 
     # sequence length 300 is arbitrary here
     seq_len = 300
-    num_channels = 20
+    num_channels = 8
     num_multi_attention_heads = 2
     model = Model(seq_len, num_channels, num_multi_attention_heads)
 
@@ -289,6 +290,9 @@ if __name__ == '__main__':
 
         # rest should be fairly self explanatory here
         pos, neg, pos_labels, neg_labels = sim_data.simulate(300, 50000, True)
+
+        # try making fastas for testing with real satori
+        # seqs = [map_decode(seq) for seq in batch_seqs.numpy()]
 
         train_X = tf.concat([pos, neg], axis=0)
         train_y = tf.concat([pos_labels, neg_labels], axis=0)
@@ -387,7 +391,7 @@ if __name__ == '__main__':
     history = model.fit(train_X, train_y, BATCH_SZ, NUM_EPOCHS, validation_data=(val_X, val_y))
     # model.show_figures(history.history)
 
-    model.save("simulated_results/disconnected_random/model_promoters.hd5")
+    model.save("simulated_results/disconnected_random/model_simulate_test.hd5")
 
     # model.show_figures(history.history)
     # testing
@@ -404,7 +408,8 @@ if __name__ == '__main__':
 
     print(model.test_on_batch(test_X, test_y))
 
-
     # print(test_y[0], model.call(tf.expand_dims(test_X[0], 0)))
     # print(test_y[5], model.call(tf.expand_dims(test_X[5], 0)))
     # print(test_y[7], model.call(tf.expand_dims(test_X[7], 0)))
+
+    
